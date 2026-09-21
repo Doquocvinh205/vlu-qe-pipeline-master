24c00c69cf64553f89fe53f7484e7f0255cdf808
Doquocvinh205 <vinh.2378102010147@vanlanguni.vn>
2026-09-21 14:04:11 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -20,7 +20,7 @@ function login(username, password) {
     }
 
     // Kiểm tra tài khoản và mật khẩu
-    if (username === "admin" && password === "123") {
+    if (username === "admin" && password === "1234") {
         return true;
     }
 
